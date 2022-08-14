efd91816d55614a3088959fb4ae22bef179f8327
Taisuke Fukuno <fukuno@jig.jp>
2022-08-15 07:28:35 +0900

    Initial commit

---
+++ TrustData.js
@@ -0,0 +1,71 @@
+import { Ed25519 } from "https://code4fukui.github.io/Ed25519/Ed25519.js";
+import { CBOR } from "https://js.sabae.cc/CBOR.js";
+import { TAI64N } from "https://code4fukui.github.io/TAI64N-es/TAI64N.js";
+import { subbin, bincat } from "https://js.sabae.cc/binutil.js";
+import { X25519 } from "https://code4fukui.github.io/X25519/X25519.js";
+import { convertPublicKey, convertSecretKey as convertPrivateKey } from "https://code4fukui.github.io/ed25519-to-x25519/src/ed2curve.js";
+
+export class TrustData {
+  static makeUser() {
+    const keys = Ed25519.generateKeyPair();
+    const did = keys.publicKey;
+    const secret = subbin(keys.privateKey, 0, 32);
+    return { did, secret };
+  }
+
+  static VERSION_ED25519 = 0;
+  static encode(user, body, pre = null) {
+    const message = CBOR.encode([
+      TAI64N.now(), // 12byte
+      body,
+      pre,
+    ]);
+    const res = CBOR.encode([
+      TrustData.VERSION_ED25519,
+      user.did,
+      message,
+      Ed25519.sign({ privateKey: user.secret, message, encodeing: "binary" }),
+    ]);
+    return res;
+  }
+  static decode(bin) {
+    const res = CBOR.decode(bin);
+    if (res[0] != TrustData.VERSION_ED25519) {
+      throw new Error("unsupported version: " + res[0]);
+    }
+    const publicKey = res[1];
+    const message = res[2];
+    const signature = res[3];
+    const chk = Ed25519.verify({ signature, publicKey, message, encoding: "binary" });
+    if (!chk) {
+      throw new Error("illegal signature");
+    }
+    const [ datetime, body, pre] = CBOR.decode(message);
+    return { did: publicKey, datetime: TAI64N.toDate(datetime), body, pre };
+  }
+
+  static async encrypt(user, did, bin) {
+    const sharedKey = X25519.getSharedKey(convertPrivateKey(subbin(user.secret, 0, 32)), convertPublicKey(did));
+    const iv = crypto.getRandomValues(new Uint8Array(12));
+    const buf = await crypto.subtle.encrypt({ name: "AES-GCM", iv }, await TrustData._importKey(sharedKey), bin);
+    const crypt = new Uint8Array(buf);
+    return bincat(iv, crypt);
+  }
+  static async decrypt(user, did, ivcrypt) {
+    const sharedKey = X25519.getSharedKey(convertPrivateKey(subbin(user.secret, 0, 32)), convertPublicKey(did));
+    const iv = subbin(ivcrypt, 0, 12);
+    const crypt = subbin(ivcrypt, 12, ivcrypt.length - 12);
+    const buf = await crypto.subtle.decrypt({ name: "AES-GCM", iv }, await TrustData._importKey(sharedKey), crypt);
+    return new Uint8Array(buf);
+  }
+  static _importKey = async (key) => {
+    const canexport = true;
+    return await window.crypto.subtle.importKey(
+      "raw",
+      key,
+      { name: "AES-GCM", length: 256 },
+      canexport,
+      ["encrypt", "decrypt"]
+    );
+  };
+}
---
+++ TrustData.test.js
@@ -0,0 +1,62 @@
+import * as t from "https://deno.land/std/testing/asserts.ts";
+import { TrustData } from "./TrustData.js";
+
+Deno.test("simple flow", () => {
+  const user = TrustData.makeUser();
+  const bin = TrustData.encode(user, "test");
+  t.assertEquals(bin.length, 123);
+  const res = TrustData.decode(bin);
+  t.assertEquals(user.did, res.did);
+  t.assertEquals(res.body, "test");
+});
+Deno.test("simple flow with error", () => {
+  const user = TrustData.makeUser();
+  const bin = TrustData.encode(user, "test");
+  t.assertEquals(bin.length, 123);
+  bin[1 + 32 + 10] = 0;
+  t.assertThrows(() => {
+    TrustData.decode(bin);
+  });
+});
+Deno.test("simple flow with 2 people", () => {
+  const user1 = TrustData.makeUser();
+  const user2 = TrustData.makeUser();
+  const bin1 = TrustData.encode(user1, "test1");
+  const bin2 = TrustData.encode(user2, "test2", bin1);
+  t.assertEquals(bin1.length, 124);
+  t.assertEquals(bin2.length, 250);
+  const res2 = TrustData.decode(bin2);
+  t.assertEquals(res2.did, user2.did);
+  t.assertEquals(res2.body, "test2");
+  const res1 = TrustData.decode(res2.pre);
+  t.assertEquals(res1.did, user1.did);
+  t.assertEquals(res1.body, "test1");
+});
+Deno.test("save user", () => {
+  const user = TrustData.makeUser();
+  const bin = TrustData.encode(user, user.secret);
+  t.assertEquals(bin.length, 153);
+  const res = TrustData.decode(bin);
+  const user2 = { did: res.did, secret: res.body };
+  t.assertEquals(user, user2);
+});
+Deno.test("simple flow with JSON", () => {
+  const json = { abc: "ABC", num: 5858858, bin: new Uint8Array([1, 2, 3])};
+  const user = TrustData.makeUser();
+  const bin = TrustData.encode(user, json);
+  t.assertEquals(bin.length, 145);
+  const res = TrustData.decode(bin);
+  t.assertEquals(user.did, res.did);
+  t.assertEquals(res.body, json);
+});
+Deno.test("encrypt / decrypt", async () => {
+  const body = "abc";
+  const user1 = TrustData.makeUser();
+  const user2 = TrustData.makeUser();
+  const bin = TrustData.encode(user1, body);
+  const crypt = await TrustData.encrypt(user1, user2.did, bin);
+  const bin2 = await TrustData.decrypt(user2, user1.did, crypt);
+  t.assertEquals(bin, bin2);
+  const res = TrustData.decode(bin2);
+  t.assertEquals(res.body, "abc");
+});
